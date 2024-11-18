6fd984598db37cc4999fb7aeb47fa0605397c51c
chillkirill <quwlast@gmail.com>
2024-11-19 00:10:19 +0300

    Найдена ошибка в коде

--- Программа.py
+++ Программа.py
@@ -2,5 +2,5 @@ def unique_elements(input_list):
     return list(set(input_list))
 
 my_list = [1, 2, 2, 3, 4, 4, 5]
-unique_list = unique_elements(my_list)
+unique_ = unique_elements(my_list)
 print("Уникальные элементы: ", unique_list)
