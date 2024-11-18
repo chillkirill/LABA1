722513d2bb3da7c8b2e7cc84e0fb9270f545001e
chillkirill <quwlast@gmail.com>
2024-11-19 00:12:04 +0300

    Ошибка была исправлена

--- Программа.py
+++ Программа.py
@@ -2,5 +2,5 @@ def unique_elements(input_list):
     return list(set(input_list))
 
 my_list = [1, 2, 2, 3, 4, 4, 5]
-unique_ = unique_elements(my_list)
+unique_list = unique_elements(my_list)
 print("Уникальные элементы: ", unique_list)
